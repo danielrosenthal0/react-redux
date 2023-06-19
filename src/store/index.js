606043fb5fa443a54e36de623c6f30da3d04ba77
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-06-19 14:46:35 -0400

    migrated everything to redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,32 @@
-import { legacy_createStore as createStore } from "redux";
 
-const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+const initialState = { counter: 0, showCounter: true };
 
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-        showCounter: !state.showCounter,
-        counter: state.counter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
-  }
+});
 
-  return state;
-};
 
-const store = createStore(counterReducer);
 
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
